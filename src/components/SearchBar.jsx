25e7a3e68018be2263e4628713989837750928db
Traniel Pride <tranielmp8@gmail.com>
2021-07-16 17:05:55 -0500

    commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+
+  state = {term = ''};
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" 
+              value={this.state.term}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default SearchBar;
